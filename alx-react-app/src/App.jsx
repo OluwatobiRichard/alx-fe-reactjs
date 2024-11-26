9dcc9accf6abd9ee4bd0e06cedf71ffa5676433f
OluwatobiRichard <oluwatobirichard001@gmail.com>
2024-11-26 10:49:05 +0100

    alx-react-app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer.jsx'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <WelcomeMessage />
         <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Footer />
       </>
   );
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,12 @@
+function UserProfile({ name, age, bio }) {
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>Age: {age}</p>
+        <p>{bio}</p>
+      </div>
+    );
+  }
+  
+  export default UserProfile;
+  
