53325968333e8869cb47cb5c05bd919e97a6a106
OluwatobiRichard <oluwatobirichard001@gmail.com>
2024-11-26 10:57:47 +0100

    alx-react-app

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
-function UserProfile({ name, age, bio }) {
+const UserProfile = (props) => {
     return (
-      <div>
-        <h2>{name}</h2>
-        <p>Age: {age}</p>
-        <p>Bio: {bio}</p>
-      </div>
+        <div>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
+        </div>
     );
-  }
-  
-  export default UserProfile;
-  
+};
+
+export default UserProfile;
+
