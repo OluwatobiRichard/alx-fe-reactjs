f75be854af0709388fbb389c2a3f06d97676d619
OluwatobiRichard <oluwatobirichard001@gmail.com>
2024-11-26 10:20:11 +0100

    alx-react-app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,19 @@
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer.jsx'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage.jsx';
 
 function App() {
   return (
-    <WelcomeMessage />
-  )
+      <>
+      <div>hello</div>
+        <Header />
+        <WelcomeMessage />
+        <MainContent />
+        <Footer />
+      </>
+  );
 }
 
-export default App
+export default App;
