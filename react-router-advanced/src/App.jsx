06c622090b00630a63c10344d40c74310156e410
OluwatobiRichard <oluwatobirichard001@gmail.com>
2024-11-28 17:15:31 +0100

    react-router-advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import Blog from "./components/Blog";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/profile/*" element={<Profile />} />
-                <Route path="/blog/:postId" element={<Blog />} />
-                <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes */}
+                <Route
+                    path="/profile/*"
+                    element={
+                        <ProtectedRoute isAuth={isAuthenticated}>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
+            <button onClick={() => setIsAuthenticated(!isAuthenticated)}>
+                {isAuthenticated ? "Logout" : "Login"}
+            </button>
         </Router>
     );
 };
