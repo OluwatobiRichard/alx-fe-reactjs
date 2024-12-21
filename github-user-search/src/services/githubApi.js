1ad58aca87bf2c8335c91f1280d37d61c8a018a6
OluwatobiRichard <oluwatobirichard001@gmail.com>
2024-12-21 16:49:17 +0100

    github-user-search

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,14 +1,13 @@
-const GITHUB_API_BASE_URL = 'https://api.github.com';
+const BASE_URL = "https://api.github.com";
 
 export const fetchUserData = async (username) => {
-  try {
-    const response = await fetch(`${GITHUB_API_BASE_URL}/users/${username}`);
-    if (!response.ok) {
-      throw new Error("Looks like we cant find the user");
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+            throw new Error("Looks like we cant find the user");
+        }
+        return await response.json();
+    } catch (error) {
+        throw error;
     }
-    return await response.json();
-  } catch (error) {
-    throw error;
-  }
 };
-
